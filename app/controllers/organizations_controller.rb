9696a3cb2eb4841864488ed83ff6a201d97c0df9
Esteban Pintos <epintos7@gmail.com>
2014-05-07 13:46:47 -0300

    Added owner

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -4,6 +4,11 @@ class OrganizationsController < ApplicationController
 
   FIELDS = [:name, :description, :locality]
 
+  def create
+    @organization = CreateOrganizationContext.new(current_user).handle(resource_params.first)
+    create!
+  end
+
   def resource_params
     return [] if request.get?
     [params.require(:organization).permit(FIELDS)]
---
+++ spec/contexts/create_organization_context_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+describe CreateOrganizationContext do
+
+  let(:params) { build(:organization) }
+  let(:user) { create(:user) }
+  let(:context) { CreateOrganizationContext.new(user) }
+
+  describe '#handle' do
+
+    context 'Creating an organization' do
+
+      it 'assigns the current user' do
+        organization = context.handle(JSON.parse(params.to_json))
+        expect(organization.valid?).to be true
+        expect(organization.users).to include(user)
+      end
+
+    end
+
+  end
+
+end
